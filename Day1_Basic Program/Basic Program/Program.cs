ba5ca4f3668f111a9fbb23cb57a80330d5320ecb
Hemant Kumar Sharma <137736113+Hemantks7783@users.noreply.github.com>
2026-07-22 06:58:16 +0530

    Day1_Basic Program
    C# Day1_Basic Program

---
+++ Day1_Basic Program/Basic Program/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Basic_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to : .Net");
+            Console.WriteLine("My Name is Hemant Kumar Sharma");
+            Console.WriteLine("I am learning C#");
+            Console.WriteLine("Today is my First Day");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My Name is Hemant Kumar Sharma ");
+            Console.WriteLine("&");
+            Console.WriteLine("My City is New Delhi");
+            Console.WriteLine("Today Date is: 12-07-2026");
+        }
+    }
+}
